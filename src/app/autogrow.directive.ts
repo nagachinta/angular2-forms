766da1837e21993e6d30224d267c57e7b2e8a136
naga <cdev.me@gmail.com>
2017-03-17 14:20:41 -0700

    working autogrow directive

--- src/app/autogrow.directive.ts
+++ src/app/autogrow.directive.ts
@@ -12,9 +12,9 @@ export class AutogrowDirective {
   constructor(private el: ElementRef, private renderer : Renderer) { }
 
   onFocus(){
-		this.renderer.setElementStyle(this.el, 'width','200');
+		this.renderer.setElementStyle(this.el.nativeElement, 'width','200px');
 	}
 	onBlur(){
-		this.renderer.setElementStyle(this.el,'width','120');
+		this.renderer.setElementStyle(this.el.nativeElement,'width','120px');
 	}
 }
